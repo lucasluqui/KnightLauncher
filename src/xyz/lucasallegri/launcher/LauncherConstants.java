d5d2f243b5b2d8886ace5a9073ac9f0a2b6123ec
Lucas Allegri <lucasallegri@protonmail.com>
2020-02-14 04:19:18 -0300

    No need for this extension, we're looking for compatibility

--- src/xyz/lucasallegri/launcher/LauncherConstants.java
+++ src/xyz/lucasallegri/launcher/LauncherConstants.java
@@ -6,6 +6,6 @@ public class LauncherConstants {
 
 	public static final String VERSION = "1.0_A1";
 	public static final String ROOT_FOLDER_NAME = "KnightLauncher";
-	public static final String[] STANDALONE_CLIENT_ARGS = {"java.exe","-Dsun.java2d.d3d=false", "-Dcheck_unpacked=true", "-jar",System.getProperty("user.dir") + File.separator + "getdown-pro.jar",".","client"};
+	public static final String[] STANDALONE_CLIENT_ARGS = {"java","-Dsun.java2d.d3d=false", "-Dcheck_unpacked=true", "-jar",System.getProperty("user.dir") + File.separator + "getdown-pro.jar",".","client"};
 	
 }
