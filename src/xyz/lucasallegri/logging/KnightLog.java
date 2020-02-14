ad73871496a2b24d0c5e74bc295ace8f16111ce5
Lucas Allegri <lucasallegri@protonmail.com>
2020-02-14 15:10:35 -0300

    Whats this indenting?!

--- src/xyz/lucasallegri/logging/KnightLog.java
+++ src/xyz/lucasallegri/logging/KnightLog.java
@@ -15,42 +15,42 @@ public class KnightLog {
 	public final static Logger log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 	private static FileHandler fileHandler;
 	private static ConsoleHandler consoleHandler;
-    private static SimpleFormatter formatterTxt;
+	private static SimpleFormatter formatterTxt;
 
-    public static void setup() throws IOException {
-
-    	new File("KnightLauncher.log").delete();
-    	new File("KnightLauncher.log.lck").delete();
-    	new File("KnightLauncher.log.1").delete();
-    	
-    	// get the global logger to configure it
-        consoleHandler = new ConsoleHandler();
-        fileHandler = new FileHandler("KnightLauncher.log", true);
-
-        log.setLevel(Level.ALL);
-        fileHandler.setLevel(Level.ALL);
-        consoleHandler.setLevel(Level.ALL);
-        
-        // create a TXT formatter
-        formatterTxt = new SimpleFormatter();
-        fileHandler.setFormatter(formatterTxt);
-        log.addHandler(fileHandler);
-        log.addHandler(consoleHandler);
-        log.setUseParentHandlers(false);
-    }
+	public static void setup() throws IOException {
+	
+		new File("KnightLauncher.log").delete();
+		new File("KnightLauncher.log.lck").delete();
+		new File("KnightLauncher.log.1").delete();
+		
+		// get the global logger to configure it
+	    consoleHandler = new ConsoleHandler();
+	    fileHandler = new FileHandler("KnightLauncher.log", true);
+	
+	    log.setLevel(Level.ALL);
+	    fileHandler.setLevel(Level.ALL);
+	    consoleHandler.setLevel(Level.ALL);
+	    
+	    // create a TXT formatter
+	    formatterTxt = new SimpleFormatter();
+	    fileHandler.setFormatter(formatterTxt);
+	    log.addHandler(fileHandler);
+	    log.addHandler(consoleHandler);
+	    log.setUseParentHandlers(false);
+	}
     
-    public static void logException(Exception e) {
-    	StringWriter sw = new StringWriter();
-    	PrintWriter pw = new PrintWriter(sw);
-    	e.printStackTrace(pw);
-    	log.severe(sw.toString());
-    }
+	public static void logException(Exception e) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		e.printStackTrace(pw);
+		log.severe(sw.toString());
+	}
     
-    public static void logException(Throwable t) {
-    	StringWriter sw = new StringWriter();
-    	PrintWriter pw = new PrintWriter(sw);
-    	t.printStackTrace(pw);
-    	log.severe(sw.toString());
-    }
+	public static void logException(Throwable t) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		t.printStackTrace(pw);
+		log.severe(sw.toString());
+	}
 	
 }
