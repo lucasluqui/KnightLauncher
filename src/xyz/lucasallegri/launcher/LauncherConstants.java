0b59644a4228b22d4b2d9de9f253b48c35cba6e6
Lucas Allegri <lucasallegri@protonmail.com>
2020-02-15 20:55:44 -0300

    Prepare for PRE-RELEASE 5

--- src/xyz/lucasallegri/launcher/LauncherConstants.java
+++ src/xyz/lucasallegri/launcher/LauncherConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class LauncherConstants {
 
-	public static final String VERSION = "1.0.0-PRE5_dev";
+	public static final String VERSION = "1.0.0-PRE5";
 	public static final String ROOT_FOLDER_NAME = "KnightLauncher";
 	public static final String[] STANDALONE_CLIENT_ARGS = {"java","-Dsun.java2d.d3d=false", "-Dcheck_unpacked=true", "-jar",System.getProperty("user.dir") + File.separator + "getdown-pro.jar",".","client"};
 	
