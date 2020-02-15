78152d7e2c0e7f18bd8652d50c08fa3b5b7ff95b
Lucas Allegri <lucasallegri@protonmail.com>
2020-02-15 15:44:03 -0300

    We need to initialize it first, how dare me

--- src/xyz/lucasallegri/launcher/mods/ModList.java
+++ src/xyz/lucasallegri/launcher/mods/ModList.java
@@ -1,9 +1,10 @@
 package xyz.lucasallegri.launcher.mods;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ModList {
 	
-	public static List<Mod> installedMods = null;
+	public static List<Mod> installedMods = new ArrayList<Mod>();
 
 }
