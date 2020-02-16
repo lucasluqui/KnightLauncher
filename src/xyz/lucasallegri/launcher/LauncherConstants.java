f1e8adcbe85c0aae028bae95114c8894c6a7c654
Lucas Allegri <lucasallegri@protonmail.com>
2020-02-16 15:22:52 -0300

    Back to development

--- src/xyz/lucasallegri/launcher/LauncherConstants.java
+++ src/xyz/lucasallegri/launcher/LauncherConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class LauncherConstants {
 
-	public static final String VERSION = "1.0.0-PRE8";
+	public static final String VERSION = "1.0.0-PRE9";
 	public static final String ROOT_FOLDER_NAME = "KnightLauncher";
 	public static final String[] STANDALONE_CLIENT_ARGS = {"java","-Dsun.java2d.d3d=false", "-Dcheck_unpacked=true", "-jar",System.getProperty("user.dir") + File.separator + "getdown-pro.jar",".","client"};
 	
