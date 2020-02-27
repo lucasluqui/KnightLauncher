19438bcdf3e87a47c0d5b83ce307981640d4ef0a
Lucas Allegri <lucasallegri@protonmail.com>
2020-02-27 16:06:03 -0300

    Changes to DialogError.push()

--- src/xyz/lucasallegri/dialog/DialogError.java
+++ src/xyz/lucasallegri/dialog/DialogError.java
@@ -2,15 +2,14 @@ package xyz.lucasallegri.dialog;
 
 import javax.swing.JOptionPane;
 
-import xyz.lucasallegri.launcher.LauncherGUI;
-
 public class DialogError {
 	
 	public static void push(String msg) {
-		JOptionPane.showMessageDialog(LauncherGUI.launcherGUIFrame,
+		JOptionPane.showMessageDialog(null,
 			    msg,
 			    "KnightLauncher Error",
-			    JOptionPane.ERROR_MESSAGE);
+			    JOptionPane.ERROR_MESSAGE
+			    );
 	}
 	
 }
