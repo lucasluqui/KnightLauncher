2aaddd030d0e4252519d29c1c50a90115e754c17
Lucas Allegri <lucasallegri@protonmail.com>
2020-02-27 13:02:08 -0300

    Can you please indent correctly

--- src/xyz/lucasallegri/util/SystemUtil.java
+++ src/xyz/lucasallegri/util/SystemUtil.java
@@ -4,17 +4,17 @@ public class SystemUtil {
 	
 	private static String OS = System.getProperty("os.name").toLowerCase();
 	
-    public static boolean isWindows() {
-        return OS.contains("win");
-    }
-
-    public static boolean isMac() {
-        return OS.contains("mac");
-    }
-
-    public static boolean isUnix() {
-        return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
-    }
+	public static boolean isWindows() {
+		return OS.contains("win");
+	}
+	
+	public static boolean isMac() {
+		return OS.contains("mac");
+	}
+	
+	public static boolean isUnix() {
+		return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
+	}
     
 	public static boolean is64Bit() {
 		boolean is64Bit;
