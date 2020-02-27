31e53b8a08e41f1867d743fe2552176eb57fb234
Lucas Allegri <lucasallegri@protonmail.com>
2020-02-27 12:59:42 -0300

    addition of is64Bit() function

--- src/xyz/lucasallegri/util/SystemUtil.java
+++ src/xyz/lucasallegri/util/SystemUtil.java
@@ -15,5 +15,15 @@ public class SystemUtil {
     public static boolean isUnix() {
         return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
     }
+    
+    public static boolean is64Bit() {
+	    boolean is64Bit;
+    	if (isWindows()) {
+    		is64Bit = (System.getenv("ProgramFiles(x86)") != null);
+	    } else {
+	    	is64Bit = (System.getProperty("os.arch").indexOf("64") != -1);
+	    }
+    	return is64Bit;
+    }
 
 }
