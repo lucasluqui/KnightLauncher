7b09eb19ad8113db5dfa4812bbc3b6a9095d1978
Lucas Allegri <lucasallegri@protonmail.com>
2020-03-01 16:12:21 -0300

    Add translated error dialogs

--- src/xyz/lucasallegri/dialog/DialogError.java
+++ src/xyz/lucasallegri/dialog/DialogError.java
@@ -9,6 +9,14 @@ public class DialogError {
 	public static void push(String msg) {
 		JOptionPane.showMessageDialog(null,
 			    msg,
+			    "KnightLauncher Error",
+			    JOptionPane.ERROR_MESSAGE
+			    );
+	}
+	
+	public static void pushTranslated(String msg) {
+		JOptionPane.showMessageDialog(null,
+			    msg,
 			    Language.getValue("t.error"),
 			    JOptionPane.ERROR_MESSAGE
 			    );
