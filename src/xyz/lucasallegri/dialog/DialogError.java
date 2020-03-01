6d6691a7af50f4e98c6e85bbd1de2d5cb1e768e9
Lucas Allegri <lucasallegri@protonmail.com>
2020-03-01 15:10:52 -0300

    t.error string

--- src/xyz/lucasallegri/dialog/DialogError.java
+++ src/xyz/lucasallegri/dialog/DialogError.java
@@ -2,12 +2,14 @@ package xyz.lucasallegri.dialog;
 
 import javax.swing.JOptionPane;
 
+import xyz.lucasallegri.launcher.Language;
+
 public class DialogError {
 	
 	public static void push(String msg) {
 		JOptionPane.showMessageDialog(null,
 			    msg,
-			    "KnightLauncher Error",
+			    Language.getValue("t.error"),
 			    JOptionPane.ERROR_MESSAGE
 			    );
 	}
