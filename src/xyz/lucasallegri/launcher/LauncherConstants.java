09372b12165f65947578f6c772fffa00415fca55
Lucas Allegri <lucasallegri@protonmail.com>
2020-03-05 00:42:38 -0300

    Prepare for release 1.0.2

--- src/xyz/lucasallegri/launcher/LauncherConstants.java
+++ src/xyz/lucasallegri/launcher/LauncherConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class LauncherConstants {
 
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 	public static final String[] STANDALONE_CLIENT_ARGS = {"java","-Dsun.java2d.d3d=false", "-Dcheck_unpacked=true", "-jar",System.getProperty("user.dir") + File.separator + "getdown-pro.jar",".","client"};
 	public static final String GET_MODS_URL = "https://sites.google.com/view/spiralknights-mods/mods";
 	public static final String LNK_FILE_NAME = "Knight Launcher";
