1e0ea083918aa825fc780c2d70f63548a96cb563
Lucas Allegri <lucasallegri@protonmail.com>
2020-03-24 05:16:00 -0300

    Adjusted SettingsGUI width

--- src/xyz/lucasallegri/launcher/settings/SettingsGUI.java
+++ src/xyz/lucasallegri/launcher/settings/SettingsGUI.java
@@ -63,7 +63,7 @@ public class SettingsGUI {
 	private void initialize() {
 		settingsGUIFrame = new JFrame();
 		settingsGUIFrame.setTitle(Language.getValue("t.settings"));
-		settingsGUIFrame.setBounds(100, 100, 325, 560);
+		settingsGUIFrame.setBounds(100, 100, 310, 560);
 		settingsGUIFrame.setResizable(false);
 		settingsGUIFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		settingsGUIFrame.getContentPane().setLayout(null);
