7325adaf2be9d1de853cb57a7102bbcc5bd98840
Lucas Allegri <lucasallegri@protonmail.com>
2020-04-25 20:39:26 -0300

    Removed unused settings

--- src/xyz/lucasallegri/launcher/settings/Settings.java
+++ src/xyz/lucasallegri/launcher/settings/Settings.java
@@ -2,13 +2,11 @@ package xyz.lucasallegri.launcher.settings;
 
 public class Settings {
 	
-	public static String apiEndpoint = "px-api.lucasallegri.xyz:5500/v1/";
 	public static String gamePlatform = "Steam";
 	public static String lang = "en";
 	public static Boolean doRebuilds = true;
 	public static Boolean keepOpen = false;
 	public static Boolean createShortcut = true;
-	public static Boolean jvmPatched = false;
 	public static String compressorUnzipMethod = "safe";
 	public static Integer compressorExtractBuffer = 8196;
 	public static Integer gameMemory = 512;
