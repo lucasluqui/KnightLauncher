16d7a1e800bf5894da5e9db5736d10059b6c771d
Lucas Allegri <lucasallegri@protonmail.com>
2020-04-28 00:22:45 -0300

    Prepare for release 1.2.6

--- src/xyz/lucasallegri/launcher/LauncherConstants.java
+++ src/xyz/lucasallegri/launcher/LauncherConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class LauncherConstants {
 
-	public static final String VERSION = "1.2.5";
+	public static final String VERSION = "1.2.6";
 	public static final String GET_MODS_URL = "https://sites.google.com/view/spiralknights-mods/mods";
 	public static final String LNK_FILE_NAME = "Knight Launcher";
 	public static final String EVENT_QUERY_URL = "https://aegis.lucasallegri.xyz/event";
