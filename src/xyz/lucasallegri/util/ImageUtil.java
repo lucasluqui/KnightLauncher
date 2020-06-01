e02b7b9c9568512ce94d3f56e5a84ad4dac1629d
Lucas Allegri <lucasallegri@protonmail.com>
2020-06-01 16:42:20 -0300

    New function for image rounded corners

--- src/xyz/lucasallegri/util/ImageUtil.java
+++ src/xyz/lucasallegri/util/ImageUtil.java
@@ -1,6 +1,12 @@
 package xyz.lucasallegri.util;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
 import javax.imageio.ImageIO;
@@ -17,11 +23,44 @@ public class ImageUtil {
 			URL _url = new URL(url);
 			image = ImageIO.read(_url);
 			image = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+			
 		} catch (IOException e) {
 			KnightLog.logException(e);
 		}
 		
 		return image;
+	}
+	
+	/*
+	 * Based on Philipp Reichart's response
+	 * @ https://stackoverflow.com/questions/7603400/how-to-make-a-rounded-corner-image-in-java
+	 */
+	public static Image addRoundedCorners(Image image, int radius) {
+		
+	    int w = image.getWidth(null);
+	    int h = image.getHeight(null);
+	    BufferedImage output = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+
+	    Graphics2D g2 = output.createGraphics();
+
+	    // This is what we want, but it only does hard-clipping, i.e. aliasing
+	    // g2.setClip(new RoundRectangle2D ...)
+
+	    // so instead fake soft-clipping by first drawing the desired clip shape
+	    // in fully opaque white with antialiasing enabled...
+	    g2.setComposite(AlphaComposite.Src);
+	    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	    g2.setColor(Color.WHITE);
+	    g2.fill(new RoundRectangle2D.Float(0, 0, w, h, radius, radius));
+
+	    // ... then compositing the image on top,
+	    // using the white shape from above as alpha source
+	    g2.setComposite(AlphaComposite.SrcAtop);
+	    g2.drawImage(image, 0, 0, null);
+
+	    g2.dispose();
+		
+		return output;
 	}
 
 }
