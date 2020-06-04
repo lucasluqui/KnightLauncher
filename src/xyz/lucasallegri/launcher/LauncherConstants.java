89a745b4637b46072ba906e40d4236582b451c24
Lucas Allegri <lucasallegri@protonmail.com>
2020-06-04 16:50:16 -0300

    Prepare for release 1.2.10

--- src/xyz/lucasallegri/launcher/LauncherConstants.java
+++ src/xyz/lucasallegri/launcher/LauncherConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class LauncherConstants {
 
-	public static final String VERSION = "1.2.9";
+	public static final String VERSION = "1.2.10";
 	public static final String LNK_FILE_NAME = "Knight Launcher";
 	public static final String EVENT_QUERY_URL = "https://upsilonapi.lucasallegri.xyz/event";
 	public static final String VERSION_QUERY_URL = "https://upsilonapi.lucasallegri.xyz/knightlauncher/version.txt";
@@ -24,30 +24,6 @@ public class LauncherConstants {
 			USER_DIR + File.separator + "getdown-pro.jar",
 			".",
 			"client"
-	};
-	
-	public static final String[] STANDALONE_CLIENT_ARGS = {"java",
-			"-classpath",
-			USER_DIR + File.separator + "./code/config.jar;" +
-			USER_DIR + File.separator + "./code/projectx-config.jar;" +
-			USER_DIR + File.separator + "./code/projectx-pcode.jar;" +
-			USER_DIR + File.separator + "./code/lwjgl.jar;" +
-			USER_DIR + File.separator + "./code/lwjgl_util.jar;" +
-			USER_DIR + File.separator + "./code/jinput.jar;" +
-			USER_DIR + File.separator + "./code/jutils.jar;" +
-			USER_DIR + File.separator + "./code/jshortcut.jar;" +
-			USER_DIR + File.separator + "./code/commons-beanutils.jar;" +
-			USER_DIR + File.separator + "./code/commons-digester.jar;" +
-			USER_DIR + File.separator + "./code/commons-logging.jar;",
-			"-Dcom.threerings.getdown=true",
-			"-Xmx512M",
-			"-XX:+AggressiveOpts",
-			"-XX:SoftRefLRUPolicyMSPerMB=10",
-			"-Djava.library.path=" + USER_DIR + File.separator + "./native",
-			"-Dorg.lwjgl.util.NoChecks=true",
-			"-Dsun.java2d.d3d=false",
-			"-Dappdir=" + USER_DIR + File.separator + ".",
-			"-Dresource_dir=" + USER_DIR + File.separator + "./rsrc"
 	};
 	
 }
