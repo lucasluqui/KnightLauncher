fb84ea1cbf2655dc2230658a24b5dab0c54c5cbb
Lucas Allegri <lucasallegri@protonmail.com>
2020-06-13 14:40:47 -0300

    Prepare for release 1.3.0.1

--- src/xyz/lucasallegri/launcher/LauncherConstants.java
+++ src/xyz/lucasallegri/launcher/LauncherConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class LauncherConstants {
 
-	public static final String VERSION = "1.3.0";
+	public static final String VERSION = "1.3.0.1";
 	public static final String LNK_FILE_NAME = "Knight Launcher";
 	public static final String EVENT_QUERY_URL = "https://upsilonapi.lucasallegri.xyz/event";
 	public static final String VERSION_QUERY_URL = "https://upsilonapi.lucasallegri.xyz/knightlauncher/version.txt";
@@ -15,7 +15,6 @@ public class LauncherConstants {
 	public static final String GET_MODS_URL = "https://upsilonapi.lucasallegri.xyz/knightlauncher/get-mods.html";
 	public static final String JVM_DOWNLOAD_URL = "https://upsilonapi.lucasallegri.xyz/knightlauncher/jvm-download.txt";
 	public static final String USER_DIR = System.getProperty("user.dir");
-	
 	
 	public static final String[] STANDALONE_LAUNCHER_ARGS = {".\\java_vm\\bin\\java",
 			"-Dsun.java2d.d3d=false",
