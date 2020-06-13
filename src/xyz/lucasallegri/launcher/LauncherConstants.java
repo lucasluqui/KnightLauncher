207adef92cdde013d4a48eb7e9aa9c3296815e2c
Lucas Allegri <lucasallegri@protonmail.com>
2020-06-13 14:43:24 -0300

    Revert prepare for release 1.3.0.1

--- src/xyz/lucasallegri/launcher/LauncherConstants.java
+++ src/xyz/lucasallegri/launcher/LauncherConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class LauncherConstants {
 
-	public static final String VERSION = "1.3.0.1";
+	public static final String VERSION = "1.3.0";
 	public static final String LNK_FILE_NAME = "Knight Launcher";
 	public static final String EVENT_QUERY_URL = "https://upsilonapi.lucasallegri.xyz/event";
 	public static final String VERSION_QUERY_URL = "https://upsilonapi.lucasallegri.xyz/knightlauncher/version.txt";
