6039db5f5f2884ba7f8dd4dbcf6ff4998e42a3ee
Lucas Allegri <lucasallegri@protonmail.com>
2020-06-30 16:50:07 -0300

    Prepare for release 1.3.3

--- src/xyz/lucasallegri/launcher/LauncherConstants.java
+++ src/xyz/lucasallegri/launcher/LauncherConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class LauncherConstants {
 
-	public static final String VERSION = "1.3.2.2";
+	public static final String VERSION = "1.3.3";
 	public static final String LNK_FILE_NAME = "Knight Launcher";
 	public static final String EVENT_QUERY_URL = "https://upsilonapi.lucasallegri.xyz/event";
 	public static final String VERSION_QUERY_URL = "https://upsilonapi.lucasallegri.xyz/knightlauncher/version.txt";
