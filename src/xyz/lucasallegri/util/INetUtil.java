558eccea5f0eaed86d98b06f7e28745e2282adaf
Lucas Allegri <lucasallegri@protonmail.com>
2020-07-17 20:10:41 -0300

    Disable cache on 3rd party requests

--- src/xyz/lucasallegri/util/INetUtil.java
+++ src/xyz/lucasallegri/util/INetUtil.java
@@ -17,6 +17,8 @@ public class INetUtil {
 			connection
 			.setRequestProperty("User-Agent",
 			        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+			connection.setDefaultUseCaches(false);
+			connection.setUseCaches(false);
 			connection.connect();
 			
 		    BufferedReader r = new BufferedReader(new InputStreamReader(connection.getInputStream(),
