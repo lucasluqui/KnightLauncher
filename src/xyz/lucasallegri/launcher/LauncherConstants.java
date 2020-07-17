3810fc7e8b7101642735c056cdcc3c2cb394ed61
Lucas Allegri <lucasallegri@protonmail.com>
2020-07-17 18:55:03 -0300

    This is still 1.3.4

--- src/xyz/lucasallegri/launcher/LauncherConstants.java
+++ src/xyz/lucasallegri/launcher/LauncherConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class LauncherConstants {
 
-	public static final String VERSION = "1.3.4.1";
+	public static final String VERSION = "1.3.4";
 	public static final String LNK_FILE_NAME = "Knight Launcher";
 	public static final String GITHUB_API = "https://api.github.com/";
 	public static final String GITHUB_AUTHOR = "lucas-allegri/";
