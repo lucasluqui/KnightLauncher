1fe0d10a329bb9340b339b3ee6547b20b60ac514
Lucas Allegri <lucasallegri@protonmail.com>
2020-09-22 21:46:58 -0300

    Prepare for release 1.3.7.1

--- src/com/lucasallegri/launcher/LauncherConstants.java
+++ src/com/lucasallegri/launcher/LauncherConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class LauncherConstants {
 
-	public static final String VERSION = "1.3.7";
+	public static final String VERSION = "1.3.7.1";
 	public static String LATEST_RELEASE;
 	
 	public static final String SHORTCUT_FILE_NAME = "Knight Launcher";
