562f512b8cb94a9c4db82dde8f5f5c4503df3b0d
Lucas Allegri <lucasallegri@protonmail.com>
2020-09-22 21:35:33 -0300

    Colors should be final

--- src/com/lucasallegri/launcher/Colors.java
+++ src/com/lucasallegri/launcher/Colors.java
@@ -4,14 +4,14 @@ import java.awt.Color;
 
 public class Colors {
 	
-	public static Color DARK_RED = new Color(102, 0, 0);
-	public static Color DARK_GREEN = new Color(0, 102, 34);
-	public static Color BRIGHT_RED = new Color(194, 0, 0);
-	public static Color BRIGHT_GREEN = new Color(0, 194, 65);
-	public static Color INTERFACE_PRIMARY_DARK = new Color(45, 48, 56);
-	public static Color INTERFACE_TITLEBAR_DARK = new Color(34, 36, 42);
-	public static Color INTERFACE_TITLEBAR_LIGHT = new Color(230, 230, 230);
-	public static Color DISCORD_PRIMARY = new Color(114, 137, 218);
-	public static Color KOFI = new Color(255, 94, 91);
+	public static final Color DARK_RED = new Color(102, 0, 0);
+	public static final Color DARK_GREEN = new Color(0, 102, 34);
+	public static final Color BRIGHT_RED = new Color(194, 0, 0);
+	public static final Color BRIGHT_GREEN = new Color(0, 194, 65);
+	public static final Color INTERFACE_PRIMARY_DARK = new Color(45, 48, 56);
+	public static final Color INTERFACE_TITLEBAR_DARK = new Color(34, 36, 42);
+	public static final Color INTERFACE_TITLEBAR_LIGHT = new Color(230, 230, 230);
+	public static final Color DISCORD_PRIMARY = new Color(114, 137, 218);
+	public static final Color KOFI = new Color(255, 94, 91);
 
 }
