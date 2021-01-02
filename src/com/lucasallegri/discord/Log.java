2167320bd0cbd5c559720baa2e6c8ca9b0726dae
Lucas Allegri <lucasallegri@protonmail.com>
2021-01-02 12:15:36 -0300

    New Logger references for each package

---
+++ src/com/lucasallegri/discord/Log.java
@@ -0,0 +1,15 @@
+package com.lucasallegri.discord;
+
+import com.samskivert.util.Logger;
+
+/**
+ * Contains a reference to the {@link Logger} object used by the discord package.
+ */
+@SuppressWarnings("unused")
+public class Log {
+
+  /**
+   * The {@link Logger} reference.
+   */
+  public static Logger log = Logger.getLogger("com.lucasallegri.discord");
+}
---
+++ src/com/lucasallegri/launcher/settings/Log.java
@@ -0,0 +1,15 @@
+package com.lucasallegri.launcher.settings;
+
+import com.samskivert.util.Logger;
+
+/**
+ * Contains a reference to the {@link Logger} object used by the settings package.
+ */
+@SuppressWarnings("unused")
+public class Log {
+
+  /**
+   * The {@link Logger} reference.
+   */
+  public static Logger log = Logger.getLogger("com.lucasallegri.launcher.settings");
+}
