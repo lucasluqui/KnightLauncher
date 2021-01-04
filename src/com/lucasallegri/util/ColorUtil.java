aaeec58334e8788e4e91de4f1a99cba24dd6d1c2
Lucas Allegri <lucasallegri@protonmail.com>
2021-01-04 16:43:40 -0300

    Local variables can be dismissed here

--- src/com/lucasallegri/util/ColorUtil.java
+++ src/com/lucasallegri/util/ColorUtil.java
@@ -8,28 +8,23 @@ import java.awt.*;
 public class ColorUtil {
 
   public static Color getTitleBarColor() {
-    Color c = Settings.launcherStyle.equals("dark") ? Colors.INTERFACE_TITLE_BAR_DARK : Colors.INTERFACE_TITLE_BAR_LIGHT;
-    return c;
+    return Settings.launcherStyle.equals("dark") ? Colors.INTERFACE_TITLE_BAR_DARK : Colors.INTERFACE_TITLE_BAR_LIGHT;
   }
 
   public static Color getForegroundColor() {
-    Color c = Settings.launcherStyle.equals("dark") ? Color.WHITE : Color.BLACK;
-    return c;
+    return Settings.launcherStyle.equals("dark") ? Color.WHITE : Color.BLACK;
   }
 
   public static Color getBackgroundColor() {
-    Color c = Settings.launcherStyle.equals("dark") ? Colors.INTERFACE_PRIMARY_DARK : Color.WHITE;
-    return c;
+    return Settings.launcherStyle.equals("dark") ? Colors.INTERFACE_PRIMARY_DARK : Color.WHITE;
   }
 
   public static Color getGreenForegroundColor() {
-    Color c = Settings.launcherStyle.equals("dark") ? Colors.BRIGHT_GREEN : Colors.DARK_GREEN;
-    return c;
+    return Settings.launcherStyle.equals("dark") ? Colors.BRIGHT_GREEN : Colors.DARK_GREEN;
   }
 
   public static Color getRedForegroundColor() {
-    Color c = Settings.launcherStyle.equals("dark") ? Colors.BRIGHT_RED : Colors.DARK_RED;
-    return c;
+    return Settings.launcherStyle.equals("dark") ? Colors.BRIGHT_RED : Colors.DARK_RED;
   }
 
 }
