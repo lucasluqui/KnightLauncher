b7078f97ddb5ea58fca30ea3fe3d819631fc3b6e
Lucas Allegri <lucasallegri@protonmail.com>
2021-04-06 16:09:11 -0300

    Remove UCP's setting references

--- src/main/java/com/lucasallegri/launcher/settings/SettingsProperties.java
+++ src/main/java/com/lucasallegri/launcher/settings/SettingsProperties.java
@@ -12,17 +12,17 @@ public class SettingsProperties {
 
   private static final String PROP_VER = "12";
 
-  private static final Properties prop = new Properties();
-  private static final String propPath = LauncherGlobals.USER_DIR + File.separator + "KnightLauncher.properties";
+  private static final Properties _prop = new Properties();
+  private static final String _propPath = LauncherGlobals.USER_DIR + File.separator + "KnightLauncher.properties";
 
   public static void setup() {
     try {
-      if (!FileUtil.fileExists(propPath)) {
-        FileUtil.extractFileWithinJar("/config/base.properties", propPath);
-      } else if (FileUtil.fileExists(propPath) && getValue("PROP_VER") != null
+      if (!FileUtil.fileExists(_propPath)) {
+        FileUtil.extractFileWithinJar("/config/base.properties", _propPath);
+      } else if (FileUtil.fileExists(_propPath) && getValue("PROP_VER") != null
               && !getValue("PROP_VER").equals(PROP_VER)) {
         log.warning("Old PROP_VER detected, resetting properties file.");
-        FileUtil.extractFileWithinJar("/config/base.properties", propPath);
+        FileUtil.extractFileWithinJar("/config/base.properties", _propPath);
       }
     } catch (IOException e) {
       log.error(e);
@@ -31,9 +31,9 @@ public class SettingsProperties {
 
   public static String getValue(String key) {
     String value;
-    try (InputStream is = new FileInputStream(propPath)) {
-      prop.load(is);
-      value = prop.getProperty(key);
+    try (InputStream is = new FileInputStream(_propPath)) {
+      _prop.load(is);
+      value = _prop.getProperty(key);
       log.info("Request for prop key", "key", key, "value", value);
       return value;
     } catch (IOException e) {
@@ -43,9 +43,9 @@ public class SettingsProperties {
   }
 
   public static void setValue(String key, String value) {
-    try (OutputStream os = new FileOutputStream(propPath)) {
-      prop.setProperty(key, value);
-      prop.store(new FileOutputStream(propPath), null);
+    try (OutputStream os = new FileOutputStream(_propPath)) {
+      _prop.setProperty(key, value);
+      _prop.store(new FileOutputStream(_propPath), null);
       log.info("Setting new key value", "key", key, "value", value);
     } catch (IOException e) {
       log.error(e);
@@ -71,7 +71,6 @@ public class SettingsProperties {
     Settings.jvmPatched = Boolean.parseBoolean(getValue("launcher.jvm_patched"));
     Settings.ingameRPCSetup = Boolean.parseBoolean(getValue("launcher.ingameRPCSetup"));
     Settings.useIngameRPC = Boolean.parseBoolean(getValue("launcher.useIngameRPC"));
-    Settings.ucpSetup = Boolean.parseBoolean(getValue("launcher.ucpSetup"));
     log.info("Successfully loaded all settings from prop file.");
   }
 
