f35da42dd8694f0c478716fa9cd286ce6ec4f918
Lucas Allegri <lucasallegri@protonmail.com>
2021-04-13 19:59:42 -0300

    Prepare for release 1.5.2

--- src/main/java/com/lucasallegri/launcher/LauncherGlobals.java
+++ src/main/java/com/lucasallegri/launcher/LauncherGlobals.java
@@ -4,8 +4,9 @@ import java.io.File;
 
 public class LauncherGlobals {
 
-  public static final String VERSION = "1.5.1";
   public static String latestRelease = null;
+
+  public static final String VERSION = "1.5.2";
 
   public static final String SHORTCUT_FILE_NAME = "Knight Launcher";
 
@@ -24,6 +25,16 @@ public class LauncherGlobals {
   public static final String USER_DIR = System.getProperty("user.dir");
 
 
+  public static final String[] GETDOWN_ARGS = {
+          "./java/bin/java",
+          "-Dsun.java2d.d3d=false",
+          "-Dcheck_unpacked=true",
+          "-jar",
+          "./getdown-pro.jar",
+          ".",
+          "client"
+  };
+
   public static final String[] GETDOWN_ARGS_WIN = {
           ".\\java_vm\\bin\\java.exe",
           "-Dsun.java2d.d3d=false",
@@ -34,14 +45,32 @@ public class LauncherGlobals {
           "client"
   };
 
-  public static final String[] GETDOWN_ARGS = {
+  public static final String[] ALT_CLIENT_ARGS = {
           "./java/bin/java",
+          "-classpath",
+          USER_DIR + File.separator + "./code/config.jar:" +
+                  USER_DIR + File.separator + "./code/projectx-config.jar:" +
+                  USER_DIR + File.separator + "./code/projectx-pcode.jar:" +
+                  USER_DIR + File.separator + "./code/lwjgl.jar:" +
+                  USER_DIR + File.separator + "./code/lwjgl_util.jar:" +
+                  USER_DIR + File.separator + "./code/jinput.jar:" +
+                  USER_DIR + File.separator + "./code/jutils.jar:" +
+                  USER_DIR + File.separator + "./code/jshortcut.jar:" +
+                  USER_DIR + File.separator + "./code/commons-beanutils.jar:" +
+                  USER_DIR + File.separator + "./code/commons-digester.jar:" +
+                  USER_DIR + File.separator + "./code/commons-logging.jar:",
+          "-Dcom.threerings.getdown=false",
+          "-Xms256M",
+          "-Xmx512M",
+          "-XX:+AggressiveOpts",
+          "-XX:SoftRefLRUPolicyMSPerMB=10",
+          "-Djava.library.path=" + USER_DIR + File.separator + "./native",
+          "-Dorg.lwjgl.util.NoChecks=true",
           "-Dsun.java2d.d3d=false",
-          "-Dcheck_unpacked=true",
-          "-jar",
-          "./getdown-pro.jar",
-          ".",
-          "client"
+          "-Dappdir=" + USER_DIR + File.separator + ".",
+          "-Dresource_dir=" + USER_DIR + File.separator + "./rsrc",
+          "-XX:+UseStringDeduplication",
+          "com.threerings.projectx.client.ProjectXApp",
   };
 
   public static final String[] ALT_CLIENT_ARGS_WIN = {
@@ -58,34 +87,6 @@ public class LauncherGlobals {
           USER_DIR + File.separator + "./code/commons-beanutils.jar;" +
           USER_DIR + File.separator + "./code/commons-digester.jar;" +
           USER_DIR + File.separator + "./code/commons-logging.jar;",
-          "-Dcom.threerings.getdown=false",
-          "-Xms256M",
-          "-Xmx512M",
-          "-XX:+AggressiveOpts",
-          "-XX:SoftRefLRUPolicyMSPerMB=10",
-          "-Djava.library.path=" + USER_DIR + File.separator + "./native",
-          "-Dorg.lwjgl.util.NoChecks=true",
-          "-Dsun.java2d.d3d=false",
-          "-Dappdir=" + USER_DIR + File.separator + ".",
-          "-Dresource_dir=" + USER_DIR + File.separator + "./rsrc",
-          "-XX:+UseStringDeduplication",
-          "com.threerings.projectx.client.ProjectXApp",
-  };
-
-  public static final String[] ALT_CLIENT_ARGS = {
-          "./java/bin/java",
-          "-classpath",
-          USER_DIR + File.separator + "./code/config.jar:" +
-          USER_DIR + File.separator + "./code/projectx-config.jar:" +
-          USER_DIR + File.separator + "./code/projectx-pcode.jar:" +
-          USER_DIR + File.separator + "./code/lwjgl.jar:" +
-          USER_DIR + File.separator + "./code/lwjgl_util.jar:" +
-          USER_DIR + File.separator + "./code/jinput.jar:" +
-          USER_DIR + File.separator + "./code/jutils.jar:" +
-          USER_DIR + File.separator + "./code/jshortcut.jar:" +
-          USER_DIR + File.separator + "./code/commons-beanutils.jar:" +
-          USER_DIR + File.separator + "./code/commons-digester.jar:" +
-          USER_DIR + File.separator + "./code/commons-logging.jar:",
           "-Dcom.threerings.getdown=false",
           "-Xms256M",
           "-Xmx512M",
