0563aefb2131be4c8d0d7889066fbf4672cb3120
Lucas Allegri <lucasallegri@protonmail.com>
2021-04-20 19:11:56 -0300

    Set development to Knight Launcher 1.6

--- src/main/java/com/lucasallegri/launcher/LauncherGlobals.java
+++ src/main/java/com/lucasallegri/launcher/LauncherGlobals.java
@@ -6,7 +6,7 @@ public class LauncherGlobals {
 
   public static String latestRelease = null;
 
-  public static final String VERSION = "1.5.2";
+  public static final String VERSION = "1.6-DEV";
 
   public static final String SHORTCUT_FILE_NAME = "Knight Launcher";
 
