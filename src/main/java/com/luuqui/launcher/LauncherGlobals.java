82f16bdee5e635989aee9fe363cc42ab1a2da0e2
Lucas Allegri <lucasallegri@protonmail.com>
2024-02-15 09:50:07 -0300

    Update flamingo endpoint

--- src/main/java/com/luuqui/launcher/LauncherGlobals.java
+++ src/main/java/com/luuqui/launcher/LauncherGlobals.java
@@ -8,7 +8,7 @@ public class LauncherGlobals {
 
   public static final String LAUNCHER_NAME = "Knight Launcher";
 
-  public static final String FLAMINGO_ENDPOINT = "http://flamingo.lucasallegri.net";
+  public static final String FLAMINGO_ENDPOINT = "http://flamingo.luuqui.com";
 
   public static final String GITHUB_API = "https://api.github.com/";
   public static final String GITHUB_AUTHOR = "lucasluqui";
