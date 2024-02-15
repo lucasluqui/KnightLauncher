2032d23a36e2acf24c219ae94d13f011cd151a6e
Lucas Allegri <lucasallegri@protonmail.com>
2024-02-15 17:15:41 -0300

    Fix flamingo endpoint (again)

--- src/main/java/com/luuqui/launcher/LauncherGlobals.java
+++ src/main/java/com/luuqui/launcher/LauncherGlobals.java
@@ -8,7 +8,7 @@ public class LauncherGlobals {
 
   public static final String LAUNCHER_NAME = "Knight Launcher";
 
-  public static final String FLAMINGO_ENDPOINT = "http://flamingo.luuqui.com";
+  public static final String FLAMINGO_ENDPOINT = "http://flamingo.luuqui.com:6060";
 
   public static final String GITHUB_API = "https://api.github.com/";
   public static final String GITHUB_AUTHOR = "lucasluqui";
