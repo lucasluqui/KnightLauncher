da463bb7ab12f5e3fd9a506b22e59d79f928187d
r0ngks <kevinshu81@gmail.com>
2024-06-04 00:09:26 +0800

    change default non-steam launch path of java

--- src/main/java/com/luuqui/launcher/LauncherGlobals.java
+++ src/main/java/com/luuqui/launcher/LauncherGlobals.java
@@ -36,10 +36,17 @@ public class LauncherGlobals {
   static {
 
     String javaPath;
-    if (new File(USER_DIR + File.separator + "./java/bin/java").exists()) {
-      javaPath = "./java/bin/java";
-    } else {
+    if (new File(USER_DIR, "/java_vm/bin/java").exists()) {
       javaPath = "./java_vm/bin/java";
+    } else if (new File(USER_DIR, "/java_vm/bin/java.exe").exists()){
+      javaPath = "./java_vm/bin/java.exe";
+    // just in case
+    } else if (new File(USER_DIR, "/java/bin/java").exists()){
+      javaPath = "./java/bin/java";
+    } else if (new File(USER_DIR, "/java/bin/java.exe").exists()){
+      javaPath = "./java/bin/java.exe";
+    } else {
+      javaPath = "java";
     }
 
     GETDOWN_ARGS = new String[]{
