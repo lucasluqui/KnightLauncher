e0ce12add381011f47debcde7126b60d62371bd7
Lucas Allegri <lucasallegri@protonmail.com>
2024-08-05 19:35:42 -0300

    Indentation in Colors file

--- src/main/java/com/luuqui/launcher/Colors.java
+++ src/main/java/com/luuqui/launcher/Colors.java
@@ -4,27 +4,27 @@ import java.awt.*;
 
 public class Colors {
 
-  public static final Color KL = new Color(166, 63, 103);
-  public static final Color LAUNCH = new Color(0, 133, 255);
-  public static final Color UPDATE = new Color(34, 197, 94);
-  public static final Color PREMIUM = new Color(222, 150, 47);
-  public static final Color PLAYER_COUNT = new Color(34, 197, 94);
-  public static final Color DISCORD = new Color(114, 137, 218);
-  public static final Color KOFI = new Color(255, 94, 91);
+  public static final Color KL            = new Color(166, 63, 103);
+  public static final Color LAUNCH        = new Color(0, 133, 255);
+  public static final Color UPDATE        = new Color(34, 197, 94);
+  public static final Color PREMIUM       = new Color(222, 150, 47);
+  public static final Color PLAYER_COUNT  = new Color(34, 197, 94);
+  public static final Color DISCORD       = new Color(114, 137, 218);
+  public static final Color KOFI          = new Color(255, 94, 91);
 
-  public static final Color DARK_RED = new Color(102, 0, 0);
-  public static final Color DARK_GREEN = new Color(0, 102, 34);
-  public static final Color MID_RED = new Color(247, 59, 59);
-  public static final Color MID_GREEN = new Color(52, 219, 103);
-  public static final Color BRIGHT_RED = new Color(194, 0, 0);
-  public static final Color BRIGHT_GREEN = new Color(0, 194, 65);
+  public static final Color DARK_RED      = new Color(102, 0, 0);
+  public static final Color DARK_GREEN    = new Color(0, 102, 34);
+  public static final Color MID_RED       = new Color(247, 59, 59);
+  public static final Color MID_GREEN     = new Color(52, 219, 103);
+  public static final Color BRIGHT_RED    = new Color(194, 0, 0);
+  public static final Color BRIGHT_GREEN  = new Color(0, 194, 65);
 
-  public static final Color INTERFACE_TITLE_BAR = new Color(34, 36, 42);
-  public static final Color INTERFACE_SIDEPANE_BACKGROUND = new Color(45, 48, 57);
-  public static final Color INTERFACE_SIDEPANE_BUTTON = new Color(107, 114, 128);
-  public static final Color INTERFACE_MAINPANE_BACKGROUND = new Color(56, 60, 71);
-  public static final Color INTERFACE_MAINPANE_BUTTON = new Color(INTERFACE_MAINPANE_BACKGROUND.getRed(), INTERFACE_MAINPANE_BACKGROUND.getGreen(), INTERFACE_MAINPANE_BACKGROUND.getBlue(), 150);
-  public static final Color INTERFACE_MODLIST_BACKGROUND_LIGHT = new Color(45, 48, 57);
-  public static final Color INTERFACE_MODLIST_BACKGROUND_DARK = new Color(34, 36, 43);
+  public static final Color INTERFACE_TITLE_BAR                 = new Color(34, 36, 42);
+  public static final Color INTERFACE_SIDEPANE_BACKGROUND       = new Color(45, 48, 57);
+  public static final Color INTERFACE_SIDEPANE_BUTTON           = new Color(107, 114, 128);
+  public static final Color INTERFACE_MAINPANE_BACKGROUND       = new Color(56, 60, 71);
+  public static final Color INTERFACE_MAINPANE_BUTTON           = new Color(INTERFACE_MAINPANE_BACKGROUND.getRed(), INTERFACE_MAINPANE_BACKGROUND.getGreen(), INTERFACE_MAINPANE_BACKGROUND.getBlue(), 150);
+  public static final Color INTERFACE_MODLIST_BACKGROUND_LIGHT  = new Color(45, 48, 57);
+  public static final Color INTERFACE_MODLIST_BACKGROUND_DARK   = new Color(34, 36, 43);
 
 }
