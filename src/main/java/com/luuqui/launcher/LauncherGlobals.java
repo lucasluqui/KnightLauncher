7da28c3d7aee8b0ddd90ec3bc64a4b2f55f62181
Lucas <lucasallegri@protonmail.com>
2024-09-13 17:24:33 -0300

    Prepare for release 2.0.11

--- src/main/java/com/luuqui/launcher/LauncherGlobals.java
+++ src/main/java/com/luuqui/launcher/LauncherGlobals.java
@@ -6,7 +6,7 @@ import java.io.File;
 
 public class LauncherGlobals {
 
-  public static final String LAUNCHER_VERSION = "2.0.10";
+  public static final String LAUNCHER_VERSION = "2.0.11";
 
   public static final String LAUNCHER_NAME = "Knight Launcher";
 
