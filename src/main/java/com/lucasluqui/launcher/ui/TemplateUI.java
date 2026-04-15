503a212978d46d4a4e105f0a389abf0e1c61d35b
Lucas <lucasallegri@protonmail.com>
2026-04-15 17:07:42 -0300

    Add template classes for ui and evt handler.

---
+++ src/main/java/com/lucasluqui/launcher/ui/TemplateUI.java
@@ -0,0 +1,61 @@
+/*
+package com.lucasluqui.launcher.ui;
+
+import com.google.inject.Inject;
+import com.lucasluqui.launcher.CustomColors;
+import com.lucasluqui.launcher.LocaleManager;
+
+import javax.swing.*;
+
+public class TemplateUI extends BaseUI
+{
+  @Inject
+  public TemplateUI ()
+  {
+    super(385, 460, false);
+  }
+
+  public void init ()
+  {
+    super.init();
+    compose();
+    initFinished();
+  }
+
+  public void initFinished ()
+  {
+    super.initFinished();
+  }
+
+  @Override
+  public void loadOnline ()
+  {
+
+  }
+
+  private void compose ()
+  {
+    guiFrame.setVisible(false);
+    guiFrame.setTitle(_localeManager.getValue("t.main"));
+    guiFrame.setBounds(100, 100, this.width, this.height);
+    guiFrame.setResizable(false);
+    guiFrame.setUndecorated(true);
+    guiFrame.getContentPane().setBackground(CustomColors.INTERFACE_MAINPANE_BACKGROUND);
+    guiFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    guiFrame.getContentPane().setLayout(null);
+    panel = (JPanel) guiFrame.getContentPane();
+  }
+
+  public void selectedServerChanged ()
+  {
+    this.eventHandler.selectedServerChanged();
+  }
+
+  // methods here.
+
+  @Inject public EventHandler eventHandler;
+  @Inject protected LocaleManager _localeManager;
+
+  // attributes and swing components here.
+}
+*/
---
+++ src/main/java/com/lucasluqui/launcher/ui/handler/TemplateEventHandler.java
@@ -0,0 +1,28 @@
+/*
+package com.lucasluqui.launcher.ui.handler;
+
+import com.google.inject.Inject;
+import com.lucasluqui.launcher.LocaleManager;
+
+public class TemplateEventHandler
+{
+  @Inject
+  public TemplateEventHandler (LocaleManager localeManager)
+  {
+    this._localeManager = localeManager;
+  }
+
+  public void selectedServerChanged ()
+  {
+
+  }
+
+  // methods here.
+
+  @Inject private UI ui;
+
+  protected LocaleManager _localeManager;
+
+  // attributes here.
+}
+*/
